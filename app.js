2831a4ac08cd66f8d78daead4f2e9a2a6f2087d5
Abass Shuaib <abassshuaib535@gmail.com>
2026-01-19 09:27:55 +0100

    Delete app.js

--- app.js
+++
@@ -1,8 +0,0 @@
-function showSection(id) {
-  const sections = ['home-section', 'register-section', 'login-section'];
-  sections.forEach(sec => {
-    document.getElementById(sec).classList.add('hidden');
-  });
-  document.getElementById(id).classList.remove('hidden');
-}
-
